9b69e623f5bc7bc611796f24387cca420aa555b2
mmjju <mikaelaloise.arcos@benilde.edu.ph>
2025-11-25 00:12:23 +0800

    changed the api to exchangerates from exchangerate-api. paid yung historical data for graph feature sa exchangerate-api

--- Currency Converter.py
+++ Currency Converter.py
@@ -1,29 +1,33 @@
 import requests
+import tkinter as tk
+from tkinter import messagebox
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-api_key = "18aee9556e2cfe999af9ce70"
-base_url = "https://v6.exchangerate-api.com/v6/"
+api_key = "8f96db84de0dd0181fdb9db6776bfb33"
+base_url = "http://api.exchangeratesapi.io/v1/"
 # url = "https://v6.exchangerate-api.com/v6/18aee9556e2cfe999af9ce70/latest/PHP"
 
+# API conversion
 def convert_currency(amount, base_currency, target_currency):
     try:
-        url = f"{base_url}{api_key}/latest/{base_currency}"
+        url = f"{base_url}latest?access_key={api_key}"
         response = requests.get(url)
         data = response.json()
 
         # currency rates
-        rates = data["conversion_rates"]
+        rates = data["rates"]
+        source_rate = rates[base_currency]
+        target_rate = rates[target_currency]
 
         # convert from base to target
-        target_amount = amount * rates[target_currency]
+        php_amount = amount / source_rate
+        target_amount = php_amount * target_rate
 
-        return target_amount
+        return round(target_amount, 2) # 2 decimal places
 
     except KeyError:
         return -1
-
-print(convert_currency(1, "PHP", "USD"))
-print(convert_currency(100000, "KRW", "PHP"))
-
 
 
 

4590b157516f0823294f95abbd4d4fa77f59ff4c
mmjju <mikaelaloise.arcos@benilde.edu.ph>
2025-11-25 00:58:52 +0800

    Revert "basic conversion"
    This reverts commit 7ca46f3a

--- Currency Converter.py
+++ Currency Converter.py
@@ -1,33 +1,29 @@
 import requests
-import tkinter as tk
-from tkinter import messagebox
-import matplotlib.pyplot as plt
-from datetime import datetime
 
-api_key = "8f96db84de0dd0181fdb9db6776bfb33"
-base_url = "http://api.exchangeratesapi.io/v1/"
+api_key = "18aee9556e2cfe999af9ce70"
+base_url = "https://v6.exchangerate-api.com/v6/"
 # url = "https://v6.exchangerate-api.com/v6/18aee9556e2cfe999af9ce70/latest/PHP"
 
-# API conversion
 def convert_currency(amount, base_currency, target_currency):
     try:
-        url = f"{base_url}latest?access_key={api_key}"
+        url = f"{base_url}{api_key}/latest/{base_currency}"
         response = requests.get(url)
         data = response.json()
 
         # currency rates
-        rates = data["rates"]
-        source_rate = rates[base_currency]
-        target_rate = rates[target_currency]
+        rates = data["conversion_rates"]
 
         # convert from base to target
-        php_amount = amount / source_rate
-        target_amount = php_amount * target_rate
+        target_amount = amount * rates[target_currency]
 
-        return round(target_amount, 2) # 2 decimal places
+        return target_amount
 
     except KeyError:
         return -1
+
+print(convert_currency(1, "PHP", "USD"))
+print(convert_currency(100000, "KRW", "PHP"))
+
 
 
 
